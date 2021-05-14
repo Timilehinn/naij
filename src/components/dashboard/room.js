514d743f4c905825d228b941ad6e5df220ea7be3
makinde timilehin <makindetimi@gmail.com>
2021-05-14 15:13:14 +0100

    minor change

--- src/components/dashboard/room.js
+++ src/components/dashboard/room.js
@@ -230,8 +230,8 @@ function openImg(){
                     <div style={{paddingLeft:'1rem',}}>
                         <p style={{fontSize:'.7rem',color:'grey'}}>{loading ? '' : `${refTopic[0].time} - ${refTopic[0].date}`}</p>
                     </div>
+                    <TopicFunction upvotedby={refTopic[0].upvoted_by} upvotes={refTopic[0].upvotes} topic_id={refTopic[0].id} comments={refTopic[0].comment_count} />
                 </div>)}
-                <TopicFunction upvotedby={refTopic[0].upvoted_by} upvotes={refTopic[0].upvotes} topic_id={refTopic[0].id} comments={refTopic[0].comment_count} />
                 <hr style={{color:'#5cab7d'}} />
                 {chat.map(msg=>(
                     <div key={msg.id} style={{display:'flex',flexDirection:'row',alignItems:'flex-start',paddingTop:'1rem',borderBottom:'.5px solid #5cab7d',paddingLeft:'1rem'}}>
